15c7b6863c2269f30900d7a0eb98e16c9a5c7a42
Sophie <67740809+Sophkfw@users.noreply.github.com>
2022-09-13 21:23:56 +0100

    Created JS file

---
+++ script.js
@@ -0,0 +1,5 @@
+//Test Function
+window.onload = function(){//window.addEventListener('load',function(){...}); (for Netscape) and window.attachEvent('onload',function(){...}); (for IE and Opera) also work
+    alert('JavaScript is working woo!');
+}
+
